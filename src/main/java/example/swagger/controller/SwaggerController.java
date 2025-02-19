eb0238d021d55b98ccfadb4afa0cb81fb453fc11
o-tao <dhtkdals369@gmail.com>
2025-02-20 00:12:09 +0900

    feat : @ApiResponses 적용

--- src/main/java/example/swagger/controller/SwaggerController.java
+++ src/main/java/example/swagger/controller/SwaggerController.java
@@ -2,6 +2,8 @@ package example.swagger.controller;
 
 import example.swagger.dto.controller.SwaggerRequest;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.*;
 public class SwaggerController {
 
     @Operation(summary = "Swagger 게시물 조회", description = "등록 된 Swagger 게시물을 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "응답 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
     @GetMapping
     public String getSwagger() {
         return "swagger example";
