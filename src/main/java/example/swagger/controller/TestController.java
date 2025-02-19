c96ebdc1c12be1462b108314746fbcd830bdb055
o-tao <dhtkdals369@gmail.com>
2025-02-20 00:30:59 +0900

    feat : @Hidden 적용

--- src/main/java/example/swagger/controller/TestController.java
+++ src/main/java/example/swagger/controller/TestController.java
@@ -1,5 +1,6 @@
 package example.swagger.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
+    @Hidden
     @GetMapping
     public String test() {
         return "test";
