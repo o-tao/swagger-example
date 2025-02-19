068d4e4a2c03ea6d0d4b62097fa4388268a030ac
o-tao <dhtkdals369@gmail.com>
2025-02-19 23:52:33 +0900

    feat : @Operation 적용

--- src/main/java/example/swagger/controller/SwaggerController.java
+++ src/main/java/example/swagger/controller/SwaggerController.java
@@ -1,6 +1,7 @@
 package example.swagger.controller;
 
 import example.swagger.dto.controller.SwaggerRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/swagger")
 public class SwaggerController {
 
+    @Operation(summary = "Swagger 게시물 조회", description = "등록 된 Swagger 게시물을 조회합니다.")
     @GetMapping
     public String getSwagger() {
         return "swagger example";
