2e7d5db4011a5e18fc4b57082bf7a8865b5e6eea
o-tao <dhtkdals369@gmail.com>
2025-02-20 00:25:35 +0900

    feat : @Schema 적용

--- src/main/java/example/swagger/dto/controller/SwaggerRequest.java
+++ src/main/java/example/swagger/dto/controller/SwaggerRequest.java
@@ -1,11 +1,14 @@
 package example.swagger.dto.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class SwaggerRequest {
 
+    @Schema(name = "이름", description = "회원 이름", example = "tao")
     private String name;
     private String gender;
+    @Schema(description = "회원 나이", example = "20", nullable = true)
     private int age;
 }
