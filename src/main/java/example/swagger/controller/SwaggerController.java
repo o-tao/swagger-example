4077a05cd748c1c183e2c708291c184e08841a17
o-tao <dhtkdals369@gmail.com>
2025-02-19 23:44:34 +0900

    feat : @Tag 적용

--- src/main/java/example/swagger/controller/SwaggerController.java
+++ src/main/java/example/swagger/controller/SwaggerController.java
@@ -1,8 +1,10 @@
 package example.swagger.controller;
 
 import example.swagger.dto.controller.SwaggerRequest;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Swagger", description = "스웨거 API")
 @RestController
 @RequestMapping("/api/swagger")
 public class SwaggerController {
